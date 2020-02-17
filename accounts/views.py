7e1802807eb9ad146d9a086f91c5e099e36fb4e0
luqmaan_local <luqmaanahmed@hotmail.com>
2020-02-17 22:40:42 +0000

    Created Login/ Index page, yet to create view.

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'base.html')  # temporary until index is developed.
+    return render(request, 'accounts/index.html')
