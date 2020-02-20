0f7adc4b8becc7d044fa5cdef627b1210d7fcdc7
luqmaan_local <luqmaanahmed@hotmail.com>
2020-02-20 23:26:04 +0000

    # temporary, just so I can debug.

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,3 +3,4 @@ from accounts import models
 
 # Register your models here.
 admin.site.register(models.Staff)  # will allow Staff to be registered on the admin site.
+admin.site.register(models.SessionEventModel)  # temporary, just so I can debug.
