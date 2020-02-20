6e5649f7b9fec55fe039a6bf9f19b90bf6030847
luqmaan_local <luqmaanahmed@hotmail.com>
2020-02-20 23:24:37 +0000

    currently, the class is not json serializable. Going to use JSONpickle, or change approach.

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout  # the authenticatio
 from django.http import HttpResponseRedirect  # let's me send the
 from django.urls import reverse  # synonymous to {% url 'argument'%} but in views instead of templates.
 from django.contrib.auth.decorators import login_required  # decorator to verify login
+from accounts import classes as my
+from accounts import models
 
 
 def index(request):
@@ -14,6 +16,8 @@ def index(request):
         if user:
             if user.is_active:  # if Django deems that this user is active, says it's successful.
                 login(request, user)  # ties the user to the session with cookies.
+                request.session['SessionEvent'] = my.SessionEvent(staff=models.Staff(user=request.user))
+                print(request.session['SessionEvent'])
                 return render(request, 'accounts/index.html', context={'message': 'Login Successful'})
             else:  # Account was inactive but authenticated.
                 return render(request, 'accounts/index.html', context={'message': 'Account was not active'})
